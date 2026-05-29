a4e2c3876002d4e10069840891552dd92b1ab907
uilacceb <qu6qu6g@gmail.com>
2026-05-28 23:44:36 -0400

    added web analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import MainLayout from "./components/layout/MainLayout";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <>
       <MainLayout />
+      <Analytics />
     </>
   );
 }
